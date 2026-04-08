c56a70b2eb7594628dd5271b3da1f86eb4eefef0
RamiroSokolowicz <rsokolowicz@udesa.edu.ar>
2026-04-08 20:26:14 -0300

    avances display peliculas

--- pi/src/components/Peliculas/Peliculas.js
+++ pi/src/components/Peliculas/Peliculas.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Peliculas from "../../screens/Peliculas/Peliculas;" 
 const api = "https://api.themoviedb.org/3/movie/popular?api_key=baa0951159508b20d0796a6a16699e51";
 
 
@@ -22,4 +23,21 @@ class Peliculas extends Component {
 
     render() {
         return (
-            
+            <section className="app-page">
+                {this.state.peliculas.map((pelicula, llave) => (
+                    <article key={llave}>
+                        <img className= "app-page-image" src={"https://image.tmdb.org/t/p/w500${pelicula.poster_path}"} />
+                        <h2>{pelicula.title}</h2>
+                        <p>{pelicula.overview}</p>
+                        <button>Ver descripción</button>
+                        <button>Ir a detalle</button>
+                    </article>
+                ))
+                }
+
+            </section>
+        );
+    }
+}
+
+export default Peliculas;
--- pi/src/screens/Peliculas/Peliculas.js
+++ pi/src/screens/Peliculas/Peliculas.js
@@ -1,17 +1,13 @@
 
 
 
-
-
-
-
-// function Peliculas() {
-  //  return (
-    //    <section className="app-page">
-      //      <h2 className="app-page-title">Peliculas</h2>
-        //    <p className="app-page-text">Esta pantalla puede mostrar el listado completo de peliculas.</p>
-       // </section>
-   // );
+function Peliculas() {
+  return (
+        <section className="app-page">
+            <h2 className="app-page-title">Peliculas</h2>
+            <p className="app-page-text">Esta pantalla puede mostrar el listado completo de peliculas.</p>
+        </section>
+    );
 }
 
 export default Peliculas;
